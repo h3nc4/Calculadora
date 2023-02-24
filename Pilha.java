35dd6cb9d22384aeb13c77be599132f5f3f63802
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2023-02-24 16:31:34 -0300

    Create Pilha.java

---
+++ Pilha.java
@@ -0,0 +1,87 @@
+/** 
+ * MIT License
+ *
+ * Copyright(c) 2023 Henrique Almeida
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+package calculadora;
+
+/**
+ * Classe Pilha
+ * 
+ * @author Henrique Almeida
+ */
+class Pilha<e> {
+
+	private Celula<e> topo;
+
+	/**
+	 * Construtor da classe Pilha
+	 */
+	Pilha() {
+		topo = new Celula<e>(null, null); // topo aponta para null
+	}
+
+	/**
+	 * Verifica se a pilha esta vazia
+	 * 
+	 * @return true se a pilha estiver vazia, false caso contrario
+	 */
+	public boolean estaVazia() {
+		return topo.item == null;
+	}
+
+	/**
+	 * Insere um item na pilha
+	 * 
+	 * @param novoItem Item a ser inserido
+	 */
+	public void empilhar(e novoItem) {
+		topo = new Celula<e>(novoItem, topo); // novo item aponta para o topo anterior e se torna o novo topo
+	}
+
+	/**
+	 * Remove um item da pilha
+	 * 
+	 * @return Item removido
+	 * @throws Exception Pilha vazia
+	 */
+	public e desempilhar() throws Exception {
+		if (estaVazia())
+			throw new Exception("Pilha vazia");
+		e retorno = topo.item; // guarda o item do topo
+		topo = topo.anterior; // atualiza o topo
+		return retorno;
+	}
+
+	/**
+	 * Retorna o item do topo da pilha
+	 * 
+	 * @return Item do topo da pilha
+	 * @throws Exception Pilha vazia
+	 */
+	public e topo() throws Exception {
+		if (estaVazia())
+			throw new Exception("Pilha vazia");
+		return topo.item;
+	}
+
+}
